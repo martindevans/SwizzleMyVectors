9ad121a65a41fe7cb0e0cff718e6a90468c851d0
Martin Evans <martindevans@gmail.com>
2015-10-07 16:20:39 +0100

    Implemented GetCorners for boundingbox
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- SwizzleMyVectors/Geometry/BoundingBox.cs
+++ SwizzleMyVectors/Geometry/BoundingBox.cs
@@ -81,7 +81,14 @@ namespace SwizzleMyVectors.Geometry
             if (corners.Length < CornerCount)
                 throw new ArgumentException("Array too small", "corners");
 
-            throw new NotImplementedException();
+            corners[0] = new Vector3(Min.X, Max.Y, Max.Z);
+            corners[1] = new Vector3(Max.X, Max.Y, Max.Z);
+            corners[2] = new Vector3(Max.X, Min.Y, Max.Z);
+            corners[3] = new Vector3(Min.X, Min.Y, Max.Z);
+            corners[4] = new Vector3(Min.X, Max.Y, Min.Z);
+            corners[5] = new Vector3(Max.X, Max.Y, Min.Z);
+            corners[6] = new Vector3(Max.X, Min.Y, Min.Z);
+            corners[7] = new Vector3(Min.X, Min.Y, Min.Z);
         }
 
         /// <summary>
